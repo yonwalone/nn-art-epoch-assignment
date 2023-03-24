93990bcbc5dde0b476e8d976d3746233034e1f29
bennid31 <b.doerflein@gmx.de>
2023-03-24 10:33:43 +0100

    Added images of post_impressionism

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ EPOCHS = [
 # "impressionism", #10297
 # "romanticism", #10242
 # "expressionism", #8244
-# "post-impressionism",
+# "post-impressionism", #4647
 # "baroque",
 # "art-nouveau-modern", # 6914
 #"surrealism", # 4309
