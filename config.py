b32686bd99db4d8db6c88fac0973debe9dfae0e9
SteffenLip <steffen.lippstadt@gmail.com>
2023-03-26 23:00:05 +0200

    clear image files for most epochs

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 EPOCHS = [
-# "realism", #14054
- "impressionism", #10297
-# "romanticism",
-# "expressionism", #8244
-# "post-impressionism",
-# "baroque",
-# "art-nouveau-modern", # 6914
-#"surrealism", # 4309
-# "symbolism", # 5302
+ "realism", #14054, after Clear (aC): 13693
+ "impressionism", #10297, aC: 10088
+ #"romanticism",
+ "expressionism", #8244, aC: 8097
+ #"post-impressionism",
+ #"baroque",
+ "art-nouveau-modern", # 6914, aC: 6820
+ "surrealism", # 4309, dann 4695
+ "symbolism", # 5302, aC: 5238
 ]
